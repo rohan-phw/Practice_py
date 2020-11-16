287d265f745c15135fc90ec383f24efa84fb42de
Rohan Pahwa <rohanphw@gmail.com>
2020-11-16 20:06:02 +0530

    Create 4.1.2.py

---
+++ 4.1.2.py
@@ -0,0 +1,8 @@
+# Rajesh is very new to writing a research article. 
+# He follows the bibliography style as first name followed by last name. 
+# But the editor wants name in reverse order, i.e. last name, first name. 
+# So write a python program to read first name and last name from users and print it in reverse order.
+name = str(input())
+surname = str(input())
+print(surname+","+name)
+
