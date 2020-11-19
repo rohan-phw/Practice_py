c2fd13ba08b4318ac91cb7a1be7f5cf6be977abd
Rohan Pahwa <rohanphw@gmail.com>
2020-11-19 19:21:52 +0530

    Create 19.1.py
    Leap Year

---
+++ 19.1.py
@@ -0,0 +1,10 @@
+
+#Given a year, determine whether it is a leap year. 
+# If it is a leap year, return the Boolean True, otherwise return False.
+year = int(input())
+
+if (year%4==0):
+    print("It's a leap year") 
+else :
+    print("It's not a leap year")
+
