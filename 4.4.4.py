8080c5fa8c01c8d2d2c51785edad68cb12194785
Rohan Pahwa <rohanphw@gmail.com>
2020-11-21 12:56:01 +0530

    Create 4.4.4.py
    Vin Diesel

---
+++ 4.4.4.py
@@ -0,0 +1,7 @@
+#Vin Diesel from Fast and Furious meets a loss of 5 % if he sells his race car for N dollars. 
+# What price must he sell the car so as to gain 5 % on that Car? 
+# Write a program in Python that can take the initial price N and output the best price (which gains 5% profit) ?
+N = int(input())
+CP = (N*100)/95
+SP = (105*CP)/100
+print(SP)
