448273741ea25f90c51d29053b976738904d921c
Rohan Pahwa <rohanphw@gmail.com>
2020-11-24 17:31:51 +0530

    Create 6.2.2.py
    print bennett

---
+++ 6.2.2.py
@@ -0,0 +1,7 @@
+#Print the following string "Welcome to Bennett University" , number of times desired by the user.
+a = eval(input())
+i = 1
+while i <=a:
+    print("Welcome to Bennett University")
+    i=i+1
+
